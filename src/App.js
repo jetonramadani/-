0943d7c3bd4d10b5c820d4c1991a9dc664e62da3
Guxim <guxim.selmani1@hotmail.com>
2021-11-27 18:33:39 +0100

    Filter components and flex disply and basic rendering of stores on cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import AddDataToDb from "./components/AddDataToDb";
 import Places from "./components/places/Places";
 import {default as axios} from "./axiosConfig";
+import StoreContainer from "./components/shops/StoreContainer";
 function App() {
   useEffect(() => {
     // ON MOUNT CONSTRUCTOR
@@ -16,6 +17,7 @@ function App() {
   return (
     <div>
       <div style={{height: "100px", background: "red"}}></div>
+      <StoreContainer/>
       {/* <AddDataToDb /> */}
       {/* <Places /> */}
     </div>

0c5efa22764341339a3e49fbce317e5e40318a93
Guxim <guxim.selmani1@hotmail.com>
2021-11-28 12:55:29 +0100

    Card's added

--- src/components/shops/Place.js
+++ src/components/shops/Place.js
@@ -1,12 +1,61 @@
 /* eslint-disable */
 
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: "0.8rem",
+        width: "100%",
+        border: "1px solid black",
+        maxWidth: "93%",
+        backgroundColor: theme.palette.background.paper,
+
+    },
+    inline: {
+        display: "inline"
+    }
+}));
+
 const Place = (props) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <h4>{props.address}</h4>
-        </div>);
+        <List className={classes.root}>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar
+                        alt="Remy Sharp"
+                        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Fone-click-shopping&psig=AOvVaw25QsFlL8d4kbE988OADYcZ&ust=1638185994686000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjGiMT8uvQCFQAAAAAdAAAAABAw"
+                    />
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Brunch this weekend?"
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                component="span"
+                                variant="body2"
+                                className={classes.inline}
+                                color="textPrimary"
+                            >
+                                Ali Connors
+                            </Typography>
+                            <br />
+                            {" — I'll be in your neighborhood doing errands this…"}
+                        </React.Fragment>
+                    }
+                />
+            </ListItem>
+        </List>
+    );
 }
+
 
 export default Place;

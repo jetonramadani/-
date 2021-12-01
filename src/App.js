9f0c181a6add564aeccfc3578c20f5e53b38304d
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-01 19:53:06 +0100

    configurin header for desktop and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AddDataToDb from "./components/AddDataToDb";
 // import {default as axios} from "./axiosConfig";
-// import StoreContainer from "./components/shops/StoreContainer";
+import StoreContainer from "./components/shops/StoreContainer";
 import DesktopHeader from "./components/header/DesktopHeader";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation
+} from "react-router-dom";
+import Home from "./components/homepage/Home";
+import About from "./components/about/About";
 function App() {
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState("home");
   useEffect(() => {
-    // ON MOUNT CONSTRUCTOR
+    if (location.pathname.startsWith("/store")) {
+      setActiveTab("store")
+    } else if (location.pathname.startsWith("/404")) {
+      setActiveTab("404");
+    } else if (location.pathname.startsWith("/about")) {
+      setActiveTab("about");
+    } else if (location.pathname === "/") {
+      setActiveTab("home");
+    } else {
+      setActiveTab("");
+    }
 
+    // ON MOUNT CONSTRUCTOR
     return () => {
       //ON DISMOUNT DESTRUCTOR
     };
   }, []);
   return (
-    <div>
-      <DesktopHeader />
-      {/* <div style={{ height: "50px", background: "red" }}></div> */}
+    <>
+      {activeTab !== "404" && <DesktopHeader onClick={(value) => setActiveTab(value)} />}
+      <div style={{ height: "80px", background: "transparent" }}></div>
+      <AddDataToDb />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/stores" element={<StoreContainer />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       {/* <AddDataToDb /> */}
-      {/* <StoreContainer /> */}
-      {/* <AddDataToDb /> */}
-      {/* <Places /> */}
-    </div>
+      {/* <Places /> */}</>
   );
 }
 

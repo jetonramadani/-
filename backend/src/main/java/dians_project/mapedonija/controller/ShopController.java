69d76f77594d997a1e78891be5eb40da886549e8
dev-marko <markospasenovski00@gmail.com>
2021-12-02 17:16:06 +0100

    Added new features to shop service and controller,implemented rest template for communication with the Firebase Realtime Database

--- backend/src/main/java/dians_project/mapedonija/controller/ShopController.java
+++ backend/src/main/java/dians_project/mapedonija/controller/ShopController.java
@@ -2,18 +2,18 @@ package dians_project.mapedonija.controller;
 
 import dians_project.mapedonija.model.Shop;
 import dians_project.mapedonija.service.ShopService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
-
-//throws InterruptedException, ExecutionException za FireBase
 
 @RestController
 @RequestMapping(path = "/shop")
 public class ShopController {
 
-    public final ShopService shopService;
+    private final ShopService shopService;
+
 
     public ShopController(ShopService shopService) {
         this.shopService = shopService;
@@ -24,9 +24,30 @@ public class ShopController {
         return shopService.createShop(shop);
     }
 
+    @GetMapping("/all")
+    public List<Shop> getAllShops() throws Exception {
+        return shopService.getAllShops();
+    }
+
     @GetMapping("/get/{id}")
-    public Shop getShop(@PathVariable String id) throws InterruptedException, ExecutionException {
-        return shopService.getShop(id);
+    public Shop getShopById(@PathVariable String id) throws InterruptedException, ExecutionException {
+        return shopService.getShopById(id);
+    }
+
+    // put the shop name in a query string
+    @GetMapping("/get")
+    public List<Shop> getShopByName(@RequestParam String name) throws ExecutionException, InterruptedException {
+        return shopService.getShopByName(name);
+    }
+
+    @GetMapping("/categories")
+    public List<String> getCategories() {
+        return shopService.getCategories();
+    }
+
+    @GetMapping("/cities")
+    public List<String> getCities() {
+        return shopService.getCities();
     }
 
     @DeleteMapping("/delete/{id}")

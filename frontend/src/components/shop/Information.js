ffca65a3c54a0e6796a2a9e5b813df7108348288
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-06 19:52:24 +0100

    Bolding labels

--- frontend/src/components/shop/Information.js
+++ frontend/src/components/shop/Information.js
@@ -3,7 +3,7 @@ import React from "react";
 const Information = ({label, value}) => {
   return (
     <div>
-      <div>{label}</div>
+      <strong>{label}</strong>
       <div>{value ? value : "/"}</div>
     </div>
   );

b09506392b086a26f523121626167d207ea53a7b
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-17 16:14:58 +0100

    Adding heroku link for api calls

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Next we make an 'instance' of it
 const instance = axios.create({
   // .. where we make our configurations
-  baseURL: "http://localhost:9999",
+  baseURL: "https://mapedonija-backend.herokuapp.com/",
 });
 
 // Where you would set stuff like your 'Authorization' header, etc ...

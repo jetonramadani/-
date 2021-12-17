07ebccc08ede8fddc3882fe66d1bb8f42177dcaf
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-17 18:10:05 +0100

    no routes and calls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,53 +18,51 @@ import {
 import Home from "./components/homepage/Home";
 import About from "./components/about/About";
 function App() {
-  const location = useLocation();
-  const [activeTab, setActiveTab] = useState("home");
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (location.pathname.startsWith("/store")) {
-      setActiveTab("store");
-    } else if (location.pathname.startsWith("/404")) {
-      setActiveTab("404");
-    } else if (location.pathname.startsWith("/about")) {
-      setActiveTab("about");
-    } else if (location.pathname === "/") {
-      setActiveTab("home");
-    } else {
-      setActiveTab("");
-    }
-    const loadEffect = async () => {
-      const cities = await axios.get("/shop/cities");
-      dispatch(dataActions.addCities(cities.data || []));
-      const categories = await axios.get("/shop/categories");
-      dispatch(dataActions.addCategories(categories.data || []));
-      const allShops = await axios.get("/shop/all");
-      dispatch(dataActions.addPlaces(allShops.data || []));
-    };
-    loadEffect();
-    // ON MOUNT CONSTRUCTOR
-    return () => {
-      //ON DISMOUNT DESTRUCTOR
-    };
-  }, []);
+  // const location = useLocation();
+  // const [activeTab, setActiveTab] = useState("home");
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   if (location.pathname.startsWith("/store")) {
+  //     setActiveTab("store");
+  //   } else if (location.pathname.startsWith("/404")) {
+  //     setActiveTab("404");
+  //   } else if (location.pathname.startsWith("/about")) {
+  //     setActiveTab("about");
+  //   } else if (location.pathname === "/") {
+  //     setActiveTab("home");
+  //   } else {
+  //     setActiveTab("");
+  //   }
+  //   const loadEffect = async () => {
+  //     const cities = await axios.get("/shop/cities");
+  //     dispatch(dataActions.addCities(cities.data || []));
+  //     const categories = await axios.get("/shop/categories");
+  //     dispatch(dataActions.addCategories(categories.data || []));
+  //     const allShops = await axios.get("/shop/all");
+  //     dispatch(dataActions.addPlaces(allShops.data || []));
+  //   };
+  //   loadEffect();
+  //   // ON MOUNT CONSTRUCTOR
+  //   return () => {
+  //     //ON DISMOUNT DESTRUCTOR
+  //   };
+  // }, []);
   return (
     <>
-      <div className="headerStyle">
+      {/* <div className="headerStyle">
         {activeTab !== "404" && (
           <DesktopHeader onClick={(value) => setActiveTab(value)} />
         )}
       </div>
-      {/* <AddDataToDb />  SO OVA SE DODADOA SITE PODATOCI U BAZA */}
 
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/stores" element={<StoreContainer />} />
         <Route path="/stores/:id" element={<SingleShop />} />
         <Route path="/" element={<Home />} />
-      </Routes>
+      </Routes> */}
+      TEST
 
-      {/* <AddDataToDb /> */}
-      {/* <Places /> */}
     </>
   );
 }

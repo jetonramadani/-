7cbe0380b6ce97c8f4bfd5144deb76eb88c2f0fa
Guxim <guxim.selmani1@hotmail.com>
2021-12-21 17:14:39 +0100

    Slider icons added

--- frontend/src/components/homepage/CategoriesSlider.js
+++ frontend/src/components/homepage/CategoriesSlider.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react'
-import { GrCart } from 'react-icons/gr';
-import IconButton from '@mui/material/IconButton';
 import './Home.css'
-import { BsArrowLeftSquareFill, BsArrowRightSquareFill, BsHeadphones, BsFillGiftFill } from 'react-icons/bs'
-import { MdOutlineBakeryDining, MdChair, MdSportsRugby } from 'react-icons/md'
-import { GiClothes, GiSonicShoes } from 'react-icons/gi'
-import { ImLibrary } from 'react-icons/im'
-import { VscTools } from 'react-icons/vsc'
+import {
+    mdiCart, mdiBaguette, mdiTshirtCrew,
+    mdiHeadphones, mdiBookshelf, mdiBasketball, mdiBedKingOutline,
+    mdiCarSeat, mdiShoeFormal, mdiGiftOutline,
+    mdiArrowLeftBox, mdiArrowRightBox
+} from '@mdi/js';
+import Icon from '@mdi/react'
 const mkCategries = [
     "Маркети",
     "Пекари",
@@ -23,16 +23,16 @@ const mkCategries = [
 
 
 const categoryIcons = {
-    "Маркети": <GrCart size="4rem" />,
-    "Пекари": <MdOutlineBakeryDining size="4rem" />,
-    "Бутицни": <GiClothes size="4rem" />,
-    "Електроника и компјутери": <BsHeadphones size="4rem" />,
-    "Книжарници": <ImLibrary size="4rem" />,
-    "Мебел": <MdChair size="4rem" />,
-    "Спортска опрема": <MdSportsRugby size="4rem" />,
-    "Авто-делови": <VscTools size="4rem" />,
-    "Обувки": <GiSonicShoes size="4rem" />,
-    "Сувенири": <BsFillGiftFill size="4rem" />
+    "Маркети": <Icon path={mdiCart} size="4rem" />,
+    "Пекари": <Icon path={mdiBaguette} size="4rem" />,
+    "Бутицни": <Icon path={mdiTshirtCrew} size="4rem" />,
+    "Електроника и компјутери": <Icon path={mdiHeadphones} size="4rem" />,
+    "Книжарници": <Icon path={mdiBookshelf} size="4rem" />,
+    "Мебел": <Icon path={mdiBedKingOutline} size="4rem" />,
+    "Спортска опрема": <Icon path={mdiBasketball} size="4rem" />,
+    "Авто-делови": <Icon path={mdiCarSeat} size="4rem" />,
+    "Обувки": <Icon path={mdiShoeFormal} size="4rem" />,
+    "Сувенири": <Icon path={mdiGiftOutline} size="4rem" />
 
 }
 const mapedCategories = mkCategries.map((category) =>
@@ -59,12 +59,12 @@ const CategoriesSlider = () => {
 
     return (
         <div className='slider'>
-            <IconButton onClick={() => setCurentPlace((prev) => prev === 0 ? categoryLength - 1 : prev - 1)} className='prevnext'><BsArrowLeftSquareFill /></IconButton>
+            <Icon size={'4rem'} path={mdiArrowLeftBox} onClick={() => setCurentPlace((prev) => prev === 0 ? categoryLength - 1 : prev - 1)} className='prevnext' />
             {siteWidth > 1 && <span className='sliderElement'>{mapedCategories[currentPlace % categoryLength]}</span>}
             {siteWidth > 1 && <span className='sliderElements'>{mapedCategories[(currentPlace + 1) % categoryLength]}</span>}
             {siteWidth > 2 && <span className='sliderElements'>{mapedCategories[(currentPlace + 2) % categoryLength]}</span>}
             {siteWidth > 3 && <span className='sliderElement1'>{mapedCategories[(currentPlace + 3) % categoryLength]}</span>}
-            <IconButton onClick={() => setCurentPlace((prev) => prev === (categoryLength - 1) ? 0 : prev + 1)} className='prevnext' ><BsArrowRightSquareFill /></IconButton>
+            <Icon size={'4rem'} path={mdiArrowRightBox} onClick={() => setCurentPlace((prev) => prev === (categoryLength - 1) ? 0 : prev + 1)} className='prevnext' />
         </div>
     )
 }

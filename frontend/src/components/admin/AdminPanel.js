54660f2ed4a0824499d4ea66e912ea74ebfcf4db
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-22 21:24:43 +0100

    isLogged in implementation. close #13

--- frontend/src/components/admin/AdminPanel.js
+++ frontend/src/components/admin/AdminPanel.js
@@ -1,10 +1,31 @@
-/* eslint-disable */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import classes from "./AdminPanel.module.scss";
 import EditShop from './EditShop';
+import { default as axios } from "../../axiosConfig";
+import { Navigate } from "react-router";
+
 const AdminPanel = () => {
+    const [user, setUser] = useState("");
+    useEffect(() => {
+        const effect = async () => {
+            const res = axios.get("/login/isLoggedIn");
+            if (!res.data) {
+                setUser("redirect:/login")
+            } else {
+                setUser(res.data);
+            }
+        }
+        effect();
+        return () => {
+
+        }
+    }, [])
     const places = useSelector((state) => state.data.places);
+    console.log(user)
+    if (user === "redirect:/login") {
+        return <Navigate to="/login" />
+    }
     return (
         <div className={classes.wrapper}>
             {places?.map((shop) => <EditShop key={shop.id} {...shop} />)}

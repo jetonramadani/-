b61e2991d0a9831da59e26fa298a18f4467a6775
dev-marko <markospasenovski00@gmail.com>
2022-01-30 18:28:33 +0100

    Initial commit

---
+++ authentication/src/test/java/com/dians_project/mapedonija/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dians_project.mapedonija.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
